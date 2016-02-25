7a71360085b1318c15e06ef9ed74b77409d43c14
taihejin <taihejin2008@126.com>
2016-02-25 15:22:51 +0800

    learning & fighting

---
+++ chapter3/t3.3_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+  long int estine;
+  long johns;
+  short int erns;
+  short ribs;
+  unsigned int s_count;
+  unsigned players;
+  unsigned long headcount;
+  unsigned short yesvotes;
+  long long ago;
+  int i = 2147483647;
+  unsigned int j = 4294967295;
+  printf("%d %d %d\n", i, i + 1, i + 2);
+  printf("%u %u %u\n", j, j + 1, j + 2);
+  return 0;
+}
